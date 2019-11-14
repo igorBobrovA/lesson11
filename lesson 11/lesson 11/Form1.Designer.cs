eb16098f12b176906a1894314c0bd062731d2908
igorBobrovA <57355113+igorBobrovA@users.noreply.github.com>
2019-11-14 18:51:27 +0700

    V1

---
+++ lesson 11/lesson 11/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace lesson_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.richTextBox1.Location = new System.Drawing.Point(13, 53);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(839, 282);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(864, 347);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.Text = "lesson11";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ lesson 11/lesson 11/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lesson_11
+{
+    public partial class Form1 : Form
+    {
+        Random R;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyCode == Keys.A)
+                {
+                    richTextBox1.BackColor = Color.Aqua;
+                }
+                else if (e.KeyCode == Keys.B)
+                {
+                    richTextBox1.BackColor = Color.Bisque;
+                }
+                else if (e.KeyCode == Keys.C)
+                {
+                    richTextBox1.BackColor = Color.CadetBlue;
+                }
+                else if (e.KeyCode == Keys.D)
+                {
+                    richTextBox1.BackColor = Color.DarkBlue;
+                }
+                else if (e.KeyCode == Keys.E)
+                {
+                    richTextBox1.BackColor = Color.Empty;
+                }
+                else if (e.KeyCode == Keys.F)
+                {
+                    DialogResult result = MessageBox.Show("We can't find a color on this letter,but we can install " +
+                        "a new color.\n Do you want change back color to black", "Question", MessageBoxButtons.YesNo, 
+                        MessageBoxIcon.Question);
+                        if (result == DialogResult.Yes)
+                    {
+                        richTextBox1.BackColor = Color.Black;
+                    }
+                }
+                else if (e.KeyCode == Keys.G)
+                {
+                    richTextBox1.BackColor = Color.Gold;
+                }
+                else if (e.KeyCode == Keys.H)
+                {
+                    richTextBox1.BackColor = Color.HotPink;
+                }
+                else if (e.KeyCode == Keys.I)
+                {
+                    richTextBox1.BackColor = Color.Indigo;
+                }
+                else if (e.KeyCode == Keys.J)
+                {
+                    DialogResult result = MessageBox.Show("We can't find a color on this letter,but we can install " +
+                        "a new color.\n Do you want change back color to black", "Question", MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
+                    {
+                        richTextBox1.BackColor = Color.Black;
+                    }
+                }
+                else if (e.KeyCode == Keys.K)
+                {
+                    richTextBox1.BackColor = Color.Khaki;
+                }
+                else if (e.KeyCode == Keys.L)
+                {
+                    richTextBox1.BackColor = Color.LawnGreen;
+                }
+                else if (e.KeyCode == Keys.M)
+                {
+                    richTextBox1.BackColor = Color.Maroon;
+                }
+                else if (e.KeyCode == Keys.N)
+                {
+                    richTextBox1.BackColor = Color.Navy;
+                }
+                else if (e.KeyCode == Keys.O)
+                {
+                    richTextBox1.BackColor = Color.Olive;
+                }
+                else if (e.KeyCode == Keys.P)
+                {
+                    richTextBox1.BackColor = Color.PaleGreen;
+                }
+                else if (e.KeyCode == Keys.Q)
+                {
+                    DialogResult result = MessageBox.Show("We can't find a color on this letter,but we can install " +
+                        "a new color.\n Do you want change back color to black", "Question", MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
+                    {
+                        richTextBox1.BackColor = Color.Black;
+                    }
+                }
+                else if (e.KeyCode == Keys.R)
+                {
+                    richTextBox1.BackColor = Color.Red;
+                }
+                else if (e.KeyCode == Keys.S)
+                {
+                    richTextBox1.BackColor = Color.Salmon;
+                }
+                else if (e.KeyCode == Keys.T)
+                {
+                    richTextBox1.BackColor = Color.Teal;
+                }
+                else if (e.KeyCode == Keys.U)
+                {
+                    DialogResult result = MessageBox.Show("We can't find a color on this letter,but we can install " +
+                        "a new color.\n Do you want change back color to black", "Question", MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
+                    {
+                        richTextBox1.BackColor = Color.Black;
+                    }
+                }
+                else if (e.KeyCode == Keys.V)
+                {
+                    richTextBox1.BackColor = Color.Violet;
+                }
+                else if (e.KeyCode == Keys.W)
+                {
+                    richTextBox1.BackColor = Color.White;
+                }
+                else if (e.KeyCode == Keys.X)
+                {
+                    DialogResult result = MessageBox.Show("We can't find a color on this letter,but we can install " +
+                        "a new color.\n Do you want change back color to black", "Question", MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
+                    {
+                        richTextBox1.BackColor = Color.Black;
+                    }
+                }
+                else if (e.KeyCode == Keys.Y)
+                {
+                    richTextBox1.BackColor = Color.YellowGreen;
+                }
+                else if (e.KeyCode == Keys.Z)
+                {
+                    DialogResult result = MessageBox.Show("We can't find a color on this letter,but we can install " +
+                        "a new color.\n Do you want change back color to black", "Question", MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
+                    {
+                        richTextBox1.BackColor = Color.Black;
+                    }
+                }
+                else if (e.KeyCode == Keys.Enter)
+                {
+                    if (timer1.Enabled == false)
+                    {
+                        timer1.Enabled = true;
+                    }
+                    else if (timer1.Enabled = true)
+                    {
+                        timer1.Enabled = false;
+                        richTextBox1.BackColor = Color.White;
+                    }
+                }
+                else if (e.KeyData == Keys.PageUp)
+                {
+                    timer1.Interval -= 10;
+                }
+                else if (e.KeyData == Keys.PageDown)
+                {
+                    timer1.Interval += 10;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            R = new Random();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+         richTextBox1.BackColor = Color.FromArgb(R.Next(0, 256), R.Next(0, 256), R.Next(0, 256));
+         this.BackColor = Color.FromArgb(R.Next(0, 256), R.Next(0, 256), R.Next(0, 256));
+        }
+    }
+}
